fcc87ae73626ecf64334faee685c438daf373d8d
Alexander Klauer <Graf.Zahl@gmx.net>
2012-01-04 19:31:52 +0100

    Implemented constants, rendering modes, activated RENDER_NOTHING mode

--- src/main/php/mediawiki/extensions/JHilbert/JHilbert.body.php
+++ src/main/php/mediawiki/extensions/JHilbert/JHilbert.body.php
@@ -62,6 +62,42 @@ class JHilbertException extends Exception {
  */
 class JHilbert {
 	/**
+	 * Default server location.
+	 */
+	const DEFAULT_DAEMON_IP = '127.0.0.1';
+	const DEFAULT_DAEMON_PORT = 3141;
+
+	/**
+	 * Rendering modes.
+	 */
+	const RENDER_NOTHING = 0;
+	const RENDER_MODULE = 1;
+	const RENDER_INTERFACE = 2;
+
+	/**
+	 * Server commands.
+	 * These constants must match the ones in the jhilbert.Server
+	 * java class.
+	 */
+	const COMMAND_QUIT = "\x00";
+	const COMMAND_MOD = "\x01";
+	const COMMAND_IFACE = "\x02";
+	const COMMAND_TEXT = "\x03";
+	const COMMAND_FINISH = "\x10";
+	const COMMAND_DEL = "\x20";
+
+	/**
+	 * Server responses.
+	 * These respones must match the ones in the jhilbert.Server
+	 * java class.
+	 */
+	const RESPONSE_GOODBYE = 0x00;
+	const RESPONSE_OK = 0x20;
+	const RESPONSE_MORE = 0x30;
+	const RESPONSE_CLIENT_ERR = 0x40;
+	const RESPONSE_SERVER_ERR = 0x50;
+
+	/**
 	 * Verifies the JHilbert code in a <jh>…</jh> tag.
 	 *
 	 * @param $code string JHilbert code.
@@ -74,6 +110,52 @@ class JHilbert {
 	 * @throws JHilbertException if an error occurs.
 	 */
 	public static function render( $code, array $args, Parser $parser, PPFrame $frame ) {
-		return Html::rawElement( 'pre', array(), strip_tags( $code ) );
+		try {
+			/* render according to render mode */
+			switch ( self::getRenderMode() ) {
+			case self::RENDER_MODULE:
+				$html = 'Proof module rendering not yet implemented';
+				break;
+			case self::RENDER_INTERFACE:
+				$html = 'Interface module rendering not yet implemented';
+				break;
+			default:
+				$html = Html::rawElement( 'pre', array(), strip_tags( $code ) );
+				break;
+			}
+		} catch ( JHilbertException $e ) {
+			$html = "$e";
+		}
+
+		return $html;
+	}
+
+	/**
+	 * Divines the current render mode from the page title.
+	 *
+	 * @return integer Current render mode.
+	 */
+	private static function getRenderMode() {
+		global $wgTitle;
+
+		if ( isset( $wgTitle ) ) {
+			switch ( $wgTitle->getNamespace() ) {
+			case 0:
+			case NS_USER_MODULE:
+				$mode = self::RENDER_MODULE;
+				break;
+			case NS_INTERFACE:
+			case NS_USER_INTERFACE:
+				$mode = self::RENDER_INTERFACE;
+				break;
+			default:
+				$mode = self::RENDER_NOTHING;
+				break;
+			}
+		} else {
+			$mode = self::RENDER_NOTHING;
+		}
+
+		return $mode;
 	}
 }
